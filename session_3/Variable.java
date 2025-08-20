924ca49442a906f50171b95a779313c127112be1
sergioarango25 <sergioarangogarcia8@gmail.com>
2025-08-19 21:00:33 -0500

    Archivos de sergio

---
+++ session_3/Variable.java
@@ -0,0 +1,38 @@
+class variable{
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+
+
+        byte tamino = 50;
+        short superfie = 1203;
+        Long distance = 484848483L;
+        boolean isActive = false;
+        float salary = 1.4502304015F;
+        int year = 2025;
+        double pi = 3.1338558844;
+        System.out.println("El valor de pi es" + pi);
+
+
+
+
+
+        int lados = 5;
+        int area = lados * lados;
+        System.out.print("El area de un cuadrado de es " + area );
+
+
+
+        int base = 25;
+        int altura = 10;
+        int area1 = (base * altura) / 2;
+        System.out.println("La area de un triangulo " + area1 );
+
+
+    }
+
+
+
+
+}

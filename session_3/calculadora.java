249ee1e62d5b8e0c9ed60abfc89f791a67f9bb9d
Sergio Arango Garcia <sergioarangogarcia8@gmail.com>
2025-08-26 11:44:35 -0500

    Actividad calculadora

---
+++ session_3/calculadora.java
@@ -0,0 +1,19 @@
+class calculadora {
+    public static void main(String[] args) {
+        double num1 = 6; 
+        double num2 = 2;  
+
+        double suma = num1 + num2;
+        double resta = num1 - num2;
+        double multiplicacion = num1 * num2;
+        double division =  num1 / num2;
+
+        
+
+        System.out.println(num1 + " + " + num2 + " = " + suma);
+        System.out.println(num1 + " - " + num2 + " = " + resta);
+        System.out.println(num1 + " * " + num2 + " = " + multiplicacion);
+        System.out.println(num1 + " / " + num2 + " = " + division);
+    }
+}
+
